692613536956a9cf105e7b6a85ee27c7502b0132
Oceanwall <matthew853211@gmail.com>
2018-03-30 15:21:39 -0500

    Connected backend files for word analysis
    I took methods and algorithms that I had created for another project
    and merged them into this project. Plans have been created for
    creating the CommentProcessor component.

---
+++ src/WordAnalysis/WordProcessorFunctions.js
@@ -0,0 +1,23 @@
+import { COMMON_WORDS, EXTRANEOUS, NUMBERS } from './WordAnalysisConstants.js';
+
+//Returns true if a word is common, false if it's not
+function isCommon(word) {
+  for (let i = 0; i < COMMON_WORDS.length; i++) {
+    if (COMMON_WORDS[i] == word)
+      return true;
+  }
+  return false;
+}
+
+//Returns true if a word is a contraction, false if it's not
+function isContraction(word) {
+  return word.indexOf("'") != -1 || word.indexOf("’") != -1; //checks for presence of apostrophes; if any exist, then word is assumed to be contraction
+}
+
+//Returns true if a word is extraneous, false if it's not
+//NOTE: Fix use of Numbers? or add to main array? hmm
+function checkChar(char) {
+  return EXTRANEOUS.indexOf(char) != -1 || NUMBERS.indexOf(char) != -1;
+}
+
+export { isCommon, isContraction, checkChar };
